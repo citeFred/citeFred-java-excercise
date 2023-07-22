41a1c6326f704ab4bb25b26fe4c56e3311567ff3
INYONG KIM <ohnyong87@gmail.com>
2023-07-22 20:01:14 +0900

    Docs : 신규 프로젝트 기초 파일 업로드
    - Project Name ohnyong_java
    - Language Java 선택
    - Build 시스템 Gradle 선택
    - JDK OpenJDK 11 선택

---
+++ ohnyong_java/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! 안녕하세요");
+        System.out.println("Hello world! 안녕하세요");
+        System.out.println("Hello world! 안녕하세요");
+        System.out.println("Hello world! 안녕하세요");
+    }
+}
