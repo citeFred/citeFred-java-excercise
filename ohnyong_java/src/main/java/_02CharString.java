3723a0911c515033af1d42873df0049171a851c2
INYONG KIM <ohnyong87@gmail.com>
2023-07-25 13:08:09 +0900

    test : Add test code for understanding variables
    * char와 String의 차이를 생각하기 위한 코드 실행 테스트
    * char는 기본형 타입 변수 중(+정수, 실수, 논리) 문자형임
    * String은 참조형 타입(배열, 열거, 클래스, 인터페이스 등) 중 배열이며 문자형 데이터를 참조함
    - 참조형 타입은 자료가 저장된 공간(Stack)의 주소를 저장함
    - 참조형 타입은 메모리 힙(Heap)에 실제 값을 저장함
    - 참조형 타입은 null로 초기화 할 수 있음

---
+++ ohnyong_java/src/main/java/_02CharString.java
@@ -0,0 +1,23 @@
+public class _02CharString {
+    public static void main(String[] args){
+//        char a = '안';
+//        char b = '녕';
+//        char c = '안녕';
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+
+        String c = new String("안녕");
+        String d = new String("안녕");
+        String e = "안녕";
+        String f = "안녕";
+
+        System.out.println(c);
+        System.out.println(c==d);
+        System.out.println(e==f);
+        System.out.println("hash c:"+System.identityHashCode(c));
+        System.out.println("hash d:"+System.identityHashCode(d));
+        System.out.println("hash e:"+System.identityHashCode(e));
+        System.out.println("hash f:"+System.identityHashCode(f));
+    }
+}
