0be42e52ac8c64ea459a0025069e09d2a90705ff
INYONG KIM <ohnyong87@gmail.com>
2023-07-25 22:28:09 +0900

    test : Add test code for understanding variable control
    * 기본형 변수
    - 논리boolean, 문자char, 정수byte>short>int>long, 실수float,double 값을 변수 선언 및 변수에 할당
    - 기본형 변수는 Stack에 값이 저장 됨
    * 참조형 변수
    - 문자열String, 배열Array, WrapperClass
    - 참조형 변수는 원본값이 Heap저장->값이 저장된 Stack의 주소값을 저장함
    - Boxing
    * 강제적 형변환(명시적 형변환 - Casting)
    - (타입) 을 지정하여 강제로 변환함
    * 암시적 형변환(Promotion)
    - 타입별 크기byte1>short2>int4>long8>flaot4>double8
    - (타입)을 지정안해도 크기에 따라 보다 큰 크기로 형변환이 자동으로 되는 것

---
+++ ohnyong_java/src/main/java/_03Variables.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+
+public class _03Variables {
+    public static void main(String[] args) {
+        //Primary Types
+        // 변수를 선언 해보자 => 타입 이름 = 값;
+        //1) 논리 Boolean
+        boolean flag = true;
+        System.out.println(flag);
+        flag = false;
+        System.out.println(flag);
+        //        flag = 1;
+
+        //2) 문자 char
+        //        char alphabet = a;
+        char alphabet2 = 'B';
+
+        //        System.out.println(alphabet);
+        System.out.println(alphabet2);
+
+        //3) 정수
+        byte byteNumber = 127; // -127 ~ 127(1byte)
+        //        byte byteNumber = 300; // Over
+        short shortNumber = 32767; // 32,768 ~ 32767
+        int intNumber = 2147483647;
+        long longNumber = 214783647L;
+
+        System.out.println(byteNumber);
+        System.out.println(shortNumber);
+        System.out.println(intNumber);
+        System.out.println(longNumber);
+
+        //4) 실수
+        float floatNumber = 0.123F;
+        double doubleNumber = 0.1231231;
+
+        System.out.println(floatNumber);
+        System.out.println(doubleNumber);
+
+        //Reference Types
+        //1) 문자열 변수
+        String helloWorld = "Hello World!";
+        String helloWorld2 = "Hello World!";
+
+        System.out.println(helloWorld);
+
+        System.out.println(helloWorld == helloWorld2); //Hash 값이 같다. 다르게 하려면, New로 객체 생성
+
+        String helloWorld3 = new String("helloworld");
+        String helloWorld4 = new String("helloworld");
+
+        System.out.println(helloWorld3 == helloWorld4); //Hash 값이 다르다. New로 객체 생성했기 때문에.
+
+        //2) 배열
+        int[] a = {1, 2, 3};
+
+        System.out.println(a); //주소값 Heap
+        //실제 값을 출력하려면
+        System.out.println(Arrays.toString(a));
+
+
+        //3) Wrapper Class
+        int number = 21;
+        Integer num = new Integer(number); //<- 이 과정을 boxing 한다고 함
+        //위 표현은 'Integer(int)' is deprecated 옛날 방식이라서 이렇게 안해도 된다.
+        System.out.println(num);
+
+        Integer num2 = 23;
+        System.out.println(num2);
+
+    }
+}
---
+++ ohnyong_java/src/main/java/_04TypeConversion.java
@@ -0,0 +1,77 @@
+// import
+
+import java.util.Scanner;
+
+public class _04TypeConversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //형변환
+        //ASCii Code -> TEXT
+        //        int asciiNumber = sc.nextInt();
+        //        char ch = (char)asciiNumber; // 문자로 형변환 해주면 숫자에 맞는 문자로 표현
+
+        //        System.out.println(ch);
+
+        //TEXT -> ASCii Code
+        //        char letter = sc.nextLine().charAt(0);
+        //        int asciiNumber2 = (int)letter;
+        //
+        //        System.out.println(asciiNumber2);
+        // 형 변환이란? - 변수의 타입을 바꾸는 방법
+        // 명시적 형변환(강제 형변환) Type Conversion
+        // 문자열 -> 숫자
+        // 정수 -> 실수
+        // 실수 -> 정수
+
+        // [1]double형 or float형 -> int
+        // 실수 -> 정수   (0.xxxx -> 0)
+        double doubleNumber = 10.12358;
+        float floatNumber = 13.193F;
+
+        int intNumber;
+        intNumber = (int) doubleNumber;// double -> int
+        System.out.println(intNumber);
+        int intNumber2;
+        intNumber2 = (int) floatNumber;
+        System.out.println(intNumber2);
+
+        // [2]int -> double
+        int intNumber3 = 30;
+        double doubleNumber3 = (double) intNumber3;
+        System.out.println(doubleNumber3);
+
+        ////////위 처럼 직접 지정하는 것들을 명시적인 형 변환이라 한다.
+
+        ////////암시적인 형 변환은 아래와 같다. 자동 형변환 - Promotion
+        //변수 타입별 크기 순서
+        //byte(1) -> short(2) ->int(4) ->long(8) -> float(4) ->double(8)
+        //1) 작은 크기의 타입이 큰 크기의 타입으로 변환은 암시적으로 형변환 가능하다.
+        //byte(1) ->int(4)
+        byte byteNumber = 10;
+        int intNumber4 = byteNumber; //(int)를 적지 않았지만 int로 바뀌었다.
+        System.out.println(intNumber4);
+        //char(1 byte) ->int(4)
+        char charAlphabet = 'A';
+        int intNumber6 = charAlphabet; // char ->int 자동 형변환
+        System.out.println(intNumber6);
+        int intNumber7 = 200;
+        double doubleNumber7 = intNumber7;
+        System.out.println(intNumber7);
+
+        //2) 작은 크기의 타입이 큰 크기 타입과 '계산' 될 때
+        //자동으로 큰 크기의 타입으로 형 변환 된다.
+        int intNumber10 = 10;
+        double doubleNumber10 = 5.13;
+        double result = intNumber10 + doubleNumber10;
+
+        System.out.println("Plus=>" + result);
+        //나누기
+        //정수로 나누기
+        int iResult = intNumber10 / 3;
+        //실수로 나누기
+        double dResult = intNumber10 / 12.141;
+
+        System.out.println(iResult);
+        System.out.println(dResult);
+    }
+}
