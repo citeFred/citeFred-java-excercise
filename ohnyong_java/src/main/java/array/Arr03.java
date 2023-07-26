95b75e97aaf1e899223346ffbe30ee1ce908dea1
INYONG KIM <ohnyong87@gmail.com>
2023-07-26 22:33:48 +0900

    test : Add test code for understanding *ARRAY* IMPORTANT
    * Array 복사
    - Shallow Copy
    -- 같은 주소값을 가진 상태를 그대로 복사해서 값이 변경되면 두 배열이 함께 변경되어 버린다.
    - Deep Copy
    -- 새로운 배열 객체를 생성하면서 복사하기 때문에 각자의 객채 내 주소값을 저장하여 전혀 다른 배열로 인식한다.
    - 값을 변경해도 대상 값을 바라보는 배열만 변경 됨.for문을 통해서 대입

---
+++ ohnyong_java/src/main/java/array/Arr03.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Arr03 {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4};
+        int[] b = a; // 얕은 복사(Shallow Copy)
+        System.out.println(b[0]);
+
+        b[0] = 4;
+        System.out.println(b[0]);
+        System.out.println(a[0]);
+        // Shallow Copy는 a[]와 b[]의 각 주소값이 같은 값을 바라보고 있기 때문에
+        // 1개만 변경해도 원본도 같이 변경 된다.
+
+    }
+}
---
+++ ohnyong_java/src/main/java/array/Arr04.java
@@ -0,0 +1,19 @@
+package array;
+
+import java.util.Arrays;
+
+public class Arr04 {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4};
+        int[] b = Arrays.copyOf(a, a.length); // 깊은 복사(Deep Copy)
+        System.out.println(b[0]);
+
+        b[0] = 4;
+        System.out.println(b[0]);
+        System.out.println(a[0]);
+        // Deep Copy는 a[]를 통해 새로운 배열 객체 b[]를 생성 했기 때문에
+        // 각각 주소값이 각자의 값을 바라보고 있기 때문에
+        // 1개만 변경하면 변경한 대상의 배열의 값만 변경된다.
+
+    }
+}
