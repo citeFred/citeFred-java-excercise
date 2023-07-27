401bed506991b1ee1fe584373863d575361f11cb
INYONG KIM <ohnyong87@gmail.com>
2023-07-27 17:27:22 +0900

    test : Add test code for *Set-Collection* *DANGER*
    * Set - 집합, 순서 없고, 중복 없다.
    - 순서가 보장되지 않는대신 중복을 허용하지 않는 곳에서 사용
    - 빠른 검색이 필요할 때 사용하는 클래스,같은 자료를 중복 보관할 수 없다.
    - Set 클래스(<interface임)
    - HashSet, TreeSet, LinkedHashSet이 있음
    - HashSet이 가장 성능이 좋다.
    -- Hashing을 이용해서 구현한 컬렉션
    - TreeSet
    -- 이진탐색트리(Red-Black Tree)의 형태로 데이터를 저장
    -- 데이터 추가, 삭제에는 시간이 더 걸리지만, 검색과 정렬이 뛰어나다.
    -- 기본적으로 오름차순으로 데이터를 정렬
    - LinkedHashSet
    -- HashSet 클래스를 상속받은 LinkedList
    -- 데이터에 삽입된 순서대로 데이터를 관리한다.
    - 기본 생성법
    -- Set<E> name = new HashSet<>();
    - HashSet의 메서드를 사용
    -- .add(E e) : element 추가
    -- .contains(Object o): element 존재 여부 반환 TorF

---
+++ ohnyong_java/src/main/java/_05collection/Col_03Set.java
@@ -0,0 +1,42 @@
+package _05collection;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class Col_03Set {
+    public static void main(String[] args) {
+        //Set(집합)
+        // 순서가 없고, 중복이 없다.
+        // 순서가 보장되지 않는 대신 중복을 허용하지 않도록하는 프로그램에서 사용 할 수 있는 자료구조.
+        // Set -> 그냥 쓸수도 있음,
+        // 그러나 HashSet, TreeSet 등으로 응용해서 같이 사용 가능
+        // HashSet은 Set 인터페이스를 구현한 가장 대표적인 컬렉션이다.
+        // 저장순서를 유지하고자 한다면 LinkedHashSet을 사용해야한다.
+        // Set은 생성자가 없는 껍데기라서 바로 생성 할 수 없음!!
+        // 그래서 생성자가 존재하는 HashSet을 이용해서 -> Set을 구현해볼 수 있음
+
+        //Set 선언 + (객체)생성
+        Set<Integer> intSet = new HashSet<>();
+
+        //추가(c)
+        //add(element) 메서드로 생성한 Set에 element를 넣어보자.
+        intSet.add(2);
+        intSet.add(51);
+        intSet.add(2);
+        intSet.add(6);
+        intSet.add(2);
+        intSet.add(282);
+
+        //읽기
+        for (Integer value: intSet){
+            System.out.println(value);
+        }
+        //2, 51, 6, 282 => 2가 3번 add됐지만 중복을 허용하지 않아 1개로만 나타난다.
+
+        //포함여부확인
+        //contains
+        System.out.println(intSet.contains(3));
+        System.out.println(intSet.contains(2));
+
+    }
+}
