df18f3d6f6ddd11d2cabad43f9bfec50c6001e2d
INYONG KIM <ohnyong87@gmail.com>
2023-08-01 11:19:01 +0900

    test : Make test code for code-review with ChatGPT
    * ChatGPT 코드 리뷰 전 문제 발생 상태의 코드 저장
    - 메뉴간 탈출, 진입이 진행되지 않는 상황
    - chooseMenu의 selectNum의 스코프에 대한 문제가 있는듯함
    - ChatGPT를 통해 코드 리뷰를 진행 할 예정

--- ohnyong_java/src/main/java/Algorithm/Solution.java
+++ ohnyong_java/src/main/java/Algorithm/Solution.java
@@ -21,7 +21,8 @@ public class Solution {
         System.out.println("[1]더하기");
         System.out.println("[2]빼기");
         System.out.println("[3]곱하기");
-        System.out.println("[4]나누기");
+        System.out.println("[4]나눈몫");
+        System.out.println("[5]나눈나머지");
     }
 
     //Menu 선택하는 메서드 chooseMenu()
@@ -29,34 +30,40 @@ public class Solution {
         for (; ; ) {
             if (selectNum == 1) {
                 System.out.println("더하기 연산을 시작합니다.");
+                inputNum(selectNum);
                 break;
             } else if (selectNum == 2) {
                 System.out.println("빼기 연산을 시작합니다.");
+                inputNum(selectNum);
                 break;
             } else if (selectNum == 3) {
                 System.out.println("곱하기 연산을 시작합니다.");
+                inputNum(selectNum);
                 break;
             } else if (selectNum == 4) {
-                System.out.println("나누기 연산을 시작합니다.");
+                System.out.println("나누기 몫 연산을 시작합니다.");
+                inputNum(selectNum);
+                break;
+            } else if (selectNum == 5) {
+                System.out.println("나누기 나머지 연산을 시작합니다.");
+                inputNum(selectNum);
                 break;
             } else {
                 System.out.println("잘못된 값을 입력했습니다.\n계산기를 다시 사용하시겠습니까?\n[1]예 [2]아니오");
                 int tryNum = sc.nextInt();
                 if (tryNum == 1) {
-                    showMenu();
+                    selectNum = 1;
+                    this.chooseMenu(selectNum);
+                    break;
                 } else {
                     System.out.println("프로그램을 종료합니다.");
                     exit();
                 }
-                break;
-            }
-
-        }
-            inputNum(selectNum);
-
+            }//--if
+        }//--for
     }
 
-void inputNum(int selectNum) {
+    void inputNum(int selectNum) {
         calNum = selectNum;
         //Num1 입력
         System.out.println("num1을 입력해주세요");
@@ -66,7 +73,7 @@ void inputNum(int selectNum) {
         num2 = sc.nextInt();
 
         calculate(calNum, num1, num2);
-}
+    }
 
     //프로그램 종료 선택 시 Run을 종료하는 메서드 exit()
     void exit() {
@@ -78,19 +85,20 @@ void inputNum(int selectNum) {
         if (calNum == 1) {
             result = summary(num1, num2);
             System.out.println("result : " + num1 + "+" + num2 + "=" + result);
-
         } else if (calNum == 2) {
             result = subtract(num1, num2);
             System.out.println("result : " + num1 + "-" + num2 + "=" + result);
-
         } else if (calNum == 3) {
             result = multiplication(num1, num2);
             System.out.println("result : " + num1 + "*" + num2 + "=" + result);
-
-        } else {// int == 4
-            result = division(num1, num2);
+        } else if (calNum == 4) {// int == 4
+            result = divisionQuiotient(num1, num2);
             System.out.println("result : " + num1 + "/" + num2 + "=" + result);
-
+        } else if (calNum == 5) {// int == 4
+            result = divisionRemainder(num1, num2);
+            System.out.println("result : " + num1 + "%" + num2 + "=" + result);
+        } else {
+            exit();
         }
         return result;
     }
@@ -114,9 +122,15 @@ void inputNum(int selectNum) {
         return result;
     }
 
-    //두 수의 나눈 값을 구하는 메서드 division()
-    int division(int num1, int num2) {
+    //두 수의 나눈 몫을 구하는 메서드 divisionQuiotient()
+    int divisionQuiotient(int num1, int num2) {
         result = num1 / num2;
+        return result;
+    }
+
+    //두 수의 나눈 나머지를 구하는 메서드 divisionQuiotient()
+    int divisionRemainder(int num1, int num2) {
+        result = num1 % num2;
         return result;
     }
 
