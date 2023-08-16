76987d54899469c2ff6dad8c03ea8b3645d84424
INYONG KIM <ohnyong87@gmail.com>
2023-08-16 21:52:23 +0900

    fix : Runtime error catch and Validation
    * 유효성 검사 수정 012같이 0값을 못읽던 부분
    * 입력값이 없을 때 발생하는 예외를 재입력받도록 구성
    * 기타 디버깅용 Gradle 설정 파일들 변경

--- ohnyong_java/src/main/java/JavaPrac2week/Solution.java
+++ ohnyong_java/src/main/java/JavaPrac2week/Solution.java
@@ -6,13 +6,10 @@ import java.util.Scanner;
 public class Solution {
     //CPU Hold input
     int ranNum1;
-    int calNum1;
     int ranNum2;
-    int calNum2;
     int ranNum3;
-    int calNum3;
     int tryNum;
-    int[] cpuNum;
+    int[] cpuNum = new int[3];
 
     //User Hold input
     String inputNumOrigin;
@@ -23,8 +20,8 @@ public class Solution {
     int[] playerNum;
 
     //ouput
-    String resultStr[]; //S, B
-    int outputTryNum;
+    int loseCnt = 0; // 시도 횟수
+    boolean playerWon = false; // 성공 여부
 
     Scanner sc = new Scanner(System.in);
 
@@ -34,73 +31,76 @@ public class Solution {
 
 
     void run() throws InterruptedException {
-        while (true) {
-            showTopMenu();
-            Thread.sleep(2000);
-            int[] playerNum = inputPlayer();
-            Thread.sleep(2000);
-            int[] cpuNum = inputCPU();
-            Thread.sleep(2000);
-            String[] resultStr = calculate(cpuNum, playerNum);
-            Thread.sleep(2000);
-            output(resultStr);
-            if (!tryAgain()) {
-                break;
+        boolean continueGame = true;
+        showTopMenu();
+
+        while (continueGame) {
+            try {
+                if (!playerWon) {
+                    cpuNum = inputCPU(); // 기존 cpuNum을 업데이트하여 중복 생성하지 않도록 수정
+                    tryNum++;
+                }
+
+                int[] playerNum = inputPlayer(); // 사용자의 턴에서 입력 받음
+                calculate(cpuNum, playerNum);
+                output();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("InterruptedException occurred: " + e.getMessage());
             }
+            continueGame = tryAgain();
         }
     }
 
 
+
+
+    boolean cpuNumGenerated = false;
+
     int[] inputCPU() throws InterruptedException {
+        if (!cpuNumGenerated) {
+            // 첫 번째 게임에서만 랜덤한 값을 생성하여 cpuNum 배열에 저장
+            ranNum1 = (int) Math.round((Math.random() * 9));
+            ranNum2 = (int) Math.round((Math.random() * 9));
+            ranNum3 = (int) Math.round((Math.random() * 9));
+
+            cpuNum[0] = ranNum1;
+            cpuNum[1] = ranNum2;
+            cpuNum[2] = ranNum3;
+
+            cpuNumGenerated = true;
+        }
+
         showCpuTurn();
         //Thread.sleep(2000);
-        System.out.println("========== 첫번째 공을 쳐봅니다. =========");
-        while (true) {
+
+        // 첫 번째 공을 바라볼 때만 숫자를 생성하도록 수정
+        System.out.println("========= 첫번째 공을 바라봅니다. ========");
+        while (ranNum1 > 10 || ranNum1 <= 0) {
+            showCpuMistake();
             ranNum1 = (int) Math.round((Math.random() * 9));
-            if (ranNum1 > 9 || ranNum1 < 0) {
-                showCpuMistake();
-                continue;
-            } else if (ranNum1 >= 0 || ranNum1 < 10) {
-                //System.out.println("ranNum1 생성 완료 ==>" + ranNum2);
-                break;
-            }
         }
-        //Thread.sleep(2000);
-        System.out.println("========== 두번째 공을 쳐봅니다. =========");
-        while (true) {
+
+        // 두 번째 공을 바라볼 때만 숫자를 생성하도록 수정
+        System.out.println("========= 두번째 공을 바라봅니다. ========");
+        while (ranNum1 > 10 || ranNum1 <= 0 || ranNum2 == ranNum1) {
+            showCpuMistake();
             ranNum2 = (int) Math.round((Math.random() * 9));
-            if (ranNum2 > 9 || ranNum2 < 0) {
-                showCpuMistake();
-                continue;
-            } else if (ranNum2 >= 0 || ranNum2 < 10) {
-                if (ranNum2 == ranNum1) {
-                    showCpuMistake();
-                    continue;
-                }
-                //System.out.println("ranNum2 생성 완료 ==>" + ranNum2);
-                break;
-            }
         }
-        //Thread.sleep(1000);
-        System.out.println("========== 세번째 공을 쳐봅니다. =========");
-        while (true) {
+
+        // 세 번째 공을 바라볼 때만 숫자를 생성하도록 수정
+        System.out.println("========= 세번째 공을 바라봅니다. ========");
+        while (ranNum1 > 10 || ranNum1 <= 0 || ranNum3 == ranNum1 || ranNum3 == ranNum2) {
+            showCpuMistake();
             ranNum3 = (int) Math.round((Math.random() * 9));
-            if (ranNum3 > 9 || ranNum3 < 0) {
-                showCpuMistake();
-                continue;
-            } else if (ranNum3 >= 0 || ranNum3 < 10) {
-                //System.out.println("ranNum3 생성 완료 ==>" + ranNum3);
-                if (ranNum3 == ranNum1 && ranNum3 == ranNum2) {
-                    showCpuMistake();
-                    continue;
-                }
-                break;
-            }
         }
-        int[] cpuNum = new int[3];
+
         cpuNum[0] = ranNum1;
         cpuNum[1] = ranNum2;
         cpuNum[2] = ranNum3;
+        System.out.println("cpuNum[0]: " + cpuNum[0]);
+        System.out.println("cpuNum[1]: " + cpuNum[1]);
+        System.out.println("cpuNum[2]: " + cpuNum[2]);
         return cpuNum;
     }
 
@@ -108,14 +108,25 @@ public class Solution {
         showPlayerTurn();
         while (true) {
             inputNumOrigin = sc.nextLine();
+            System.out.println("입력된 값: " + inputNumOrigin); // 입력 값을 확인하기 위해 출력
+
+            if (inputNumOrigin.isEmpty()) {
+                System.out.println("값을 입력해주세요.");
+                continue;
+            }
+
             inputNumOriginParseInt = Integer.parseInt(inputNumOrigin);
 
-            if (inputNumOriginParseInt < 100 || inputNumOriginParseInt > 999) {
+            if (inputNumOriginParseInt < 0 || inputNumOriginParseInt > 999) {
                 showPlayerMistake1();
             } else {
-                inputNum1 = Integer.parseInt(inputNumOrigin.substring(0, 1));
-                inputNum2 = Integer.parseInt(inputNumOrigin.substring(1, 2));
-                inputNum3 = Integer.parseInt(inputNumOrigin.substring(2, 3));
+                inputNum1 = inputNumOriginParseInt / 100;
+                inputNum2 = (inputNumOriginParseInt / 10) % 10;
+                inputNum3 = inputNumOriginParseInt % 10;
+
+                System.out.println("inputNum1: " + inputNum1);
+                System.out.println("inputNum2: " + inputNum2);
+                System.out.println("inputNum3: " + inputNum3);
 
                 if (inputNum1 == inputNum2 || inputNum1 == inputNum3 || inputNum2 == inputNum3) {
                     showPlayerMistake2();
@@ -124,7 +135,6 @@ public class Solution {
                 }
             }
         }
-
         int[] playerNum = new int[3];
         playerNum[0] = inputNum1;
         playerNum[1] = inputNum2;
@@ -132,52 +142,58 @@ public class Solution {
         return playerNum;
     }
 
-    String[] calculate(int[] cpuNum, int[] playerNum) {
-        String[] resultStr = new String[3];
 
-        for (int i=0;i<3;i++) {
+    void calculate(int[] cpuNum, int[] playerNum) {
+        String[] resultStr = new String[3];
+        int strikeChecker = 0; // SSS 체크용
+        for (int i = 0; i < 3; i++) {
             if (cpuNum[i] == playerNum[i]) {
                 System.out.println("================ Strike!! ================");
                 resultStr[i] = "S";
-            }else{
+                strikeChecker++;
+            } else {
                 System.out.println("================= Ball!! =================");
                 resultStr[i] = "B";
             }
         }
-        return resultStr;
+        if (strikeChecker == 3) {
+            playerWon = true; // SSS일때만 넘어가게
+        }
     }
 
-    void output(String[] resultStr) throws InterruptedException {
-        boolean flag = true;
-        for(int i = 0; i < resultStr.length; i++){
-            if (resultStr[i].equals("S")) {
-                flag = true;
-            } else if (resultStr[i].equals("B")) {
-                flag = false;
-                break;
-            }
-        }
-        if (flag){
-            System.out.println("삼진아웃! 모두 Strike!! Player가 이겼습니다.");
-        } else if (!flag) {
-            System.out.println("CPU에게 기회가있네요.. 처음부터 다시하죠?");
+    void output() throws InterruptedException {
+        if (playerWon) {
+            System.out.println("성공하셨습니다! " + tryNum + "번만에 성공하셨습니다!");
+        } else {
+            System.out.println("앗.. 잘못 던져서 쫓겨났어요, 다음 투수로 도전합니다.");
         }
     }
 
     boolean tryAgain() throws InterruptedException {
-        System.out.println("  [1] YES  /  [2] No Thanks  ");
-        tryNum = sc.nextInt(); // 전역 선언된 tryNum 변수에 값을 저장
-        sc.nextLine();
+        if (playerWon) {
+            System.out.println("재도전하시겠습니까?");
+            System.out.println("  [1] YES  /  [2] No Thanks  ");
+            tryNum = sc.nextInt();
+            sc.nextLine(); // 개행 문자 처리
 
-        if (tryNum == 1) {
-            System.out.println("게임을 다시 시작합니다.");
-            return true;
-        } else if (tryNum == 2) {
-            System.out.println("즐거운 게임이었습니다!! 좋은 하루 되세요.");
-            return false;
+            if (tryNum == 1) {
+                System.out.println("게임을 다시 시작합니다.");
+                cpuNumGenerated = false; // cpuNum 초기화
+                playerWon = false; // playerWon 초기화
+                loseCnt = 0; // 실패 횟수 초기화
+                return true;
+            } else if (tryNum == 2) {
+                System.out.println("즐거운 게임이었습니다!! 좋은 하루 되세요.");
+                return false;
+            } else {
+                System.out.println("잘못된 접근입니다. 프로그램을 종료합니다.");
+                return false;
+            }
         } else {
-            System.out.println("잘못된 접근입니다. 프로그램을 종료합니다.");
-            return false;
+            // 성공하지 못한 경우에는 바로 다시 시작
+            System.out.println("게임을 다시 시작합니다.");
+            loseCnt = 0; // 실패 횟수 초기화
+            return true;
         }
     }
     void showTopMenu() {
@@ -191,7 +207,7 @@ public class Solution {
         System.out.println("============== Player's Turn =============");
         System.out.println("==========================================");
         System.out.println("======= 3자리 숫자를 입력해 주세요 =======");
-        System.out.println("===== 숫자를 맞춰서 아웃 시킬겁니다. =====");
+        System.out.println("=========== 당신은 투수입니다. ===========");
         System.out.println("===== 중복 숫자는 허용되지 않습니다. =====");
         System.out.println("==========================================");
         System.out.print("ex)123==>");
@@ -212,6 +228,7 @@ public class Solution {
 
     void showPlayerMistake1() {
         System.out.println("=== 숫자는 3자리를 입력해주세요 ex)123 ===");
+        System.out.println("=========== 다시 입력해주세요. ===========");
     }
 
     void showPlayerMistake2() {
