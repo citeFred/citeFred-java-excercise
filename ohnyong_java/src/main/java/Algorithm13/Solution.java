27cafa6d5e4db67ff59cf4173bd258cac0e136a7
INYONG KIM <ohnyong87@gmail.com>
2023-08-17 23:11:53 +0900

    test : Algorithm test - StringBuffer.reverse() method
    * 입력받은 문자열을 거꾸로 출력하는 방법
    - StringBuffer 클래스의 reverse()메소드 사용

---
+++ ohnyong_java/src/main/java/Algorithm13/Solution.java
@@ -0,0 +1,24 @@
+package Algorithm13;
+
+
+
+class Solution {
+    static String my_string = "REVERSE TEST";
+    //Solution을 클래스 메소드로 선언
+    public static String solution(String my_string) {
+        //StringBuffer 클래스의 reverse() 메소드를 사용 해보자.
+        StringBuffer sb = new StringBuffer(my_string);
+        String answer = "";
+
+        answer = sb.reverse().toString();
+        return answer;
+    }
+    //코테 작성 부분에서 메인이 없을 수 있다. 메인 작성해주자!
+    public static void main(String[] args) {
+        //클래스 메소드(Static Method) 기 때문에
+        //객체를 생성 안해도 메소드를 호출 할 수 있다.
+        //Solution solution = new Solution();
+        String result = solution(my_string);
+        System.out.println(result);
+    }
+}
--- ohnyong_java/src/main/java/Main.java
+++ ohnyong_java/src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
+    //Q1. 90점을 넘으면 A등급, 아니라면 B등급을 출력하는 소스코드입니다. 빈칸에 들어갈 알맞은 소스코드는?
     public static void main(String[] args){
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        int value = 356;
+        System.out.println((int)(Math.floor((value/100)*100)));
     }
 }
