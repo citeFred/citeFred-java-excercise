f75e3d95a56f80e5a43753744f587b98db4e5dfc
INYONG KIM <ohnyong87@gmail.com>
2023-09-05 09:18:24 +0900

    test : Algorithm - Stream, Arrays.copyOfRange() method
    * Arrays클래스의 copyOfRange(arr,i,j) 메소드 사용
    * copyOfRange(arr,i,j)는 arr을 i부터j인덱스까지 복사한다
    * stream으로 .toArray()로 배열로 반환
    * stream 장점
    - 코드가 간결해지고 표현이 좋다
    - 하지만 성능 같은것들도 생각해봐야 한다
    - 또한 기본 for loop의 동작을 이해해야 이것도 적응할수있을것 같다

---
+++ ohnyong_java/src/main/java/Algorithm37/Solution.java
@@ -0,0 +1,51 @@
+package Algorithm37;
+
+import java.util.Arrays;
+
+/*
+K번째수
+문제 설명
+배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+
+예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
+
+array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
+1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.
+2에서 나온 배열의 3번째 숫자는 5입니다.
+배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때,
+commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+array의 길이는 1 이상 100 이하입니다.
+array의 각 원소는 1 이상 100 이하입니다.
+commands의 길이는 1 이상 50 이하입니다.
+commands의 각 원소는 길이가 3입니다.
+입출력 예
+array	commands	return
+[1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
+* */
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int[] Solution(int[] array, int[] commands){
+        int i = commands[0];
+        int j = commands[1];
+        int k = commands[2];
+        return Arrays.stream(Arrays.copyOfRange(array, i, k)).toArray(); //array의 i~k 번째 요소들을 복사해서 새로운 배열만들기
+    }
+// Arrays.copyOfRange(배열, 시작인덱스, 끝인덱스) -> 배열을 시작~끝 범위 인덱스들을 복사해서 추출
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] array = {1,5,2,6,3,7,4};
+        int i = 2;
+        int j = 5;
+        int k = 3;
+        int[] commands = {i, j, k};
+        int[] result = sol.Solution(array, commands); //를 호출해서 반환값을 result배열에 담어
+        String resultString = Arrays.toString(result); //출력하려면 문자열결과로 만들어
+        System.out.println(resultString);//출력해
+    }
+}
