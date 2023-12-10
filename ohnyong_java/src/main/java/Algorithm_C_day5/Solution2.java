dd4fe0747430af971a0c3edfe487697f92f6ffc1
INYONG KIM <ohnyong87@gmail.com>
2023-12-10 14:21:33 +0900

    test : Algorithm - 던전 스테미너 계산 완전탐색필요, 피로도

--- ohnyong_java/src/main/java/Algorithm_C_day5/Solution2.java
+++ ohnyong_java/src/main/java/Algorithm_C_day5/Solution2.java
@@ -1,28 +1,46 @@
 package Algorithm_C_day5;
 
-import java.util.Arrays;
-
 public class Solution2 {
 
-    int solution(int k, int[][] dungeons) {
-        int answer = 0;
-        int currentStamina = k;
+    public int solution(int k, int[][] dungeons) {
+        /*
+        ABC 이렇게 있다면
+        ABC ACB BAC BCA CAB CBA 이렇게 던전을 갈 수 있다.
 
-        // 던전을 "소모 피로도" 기준으로 오름차순 정렬
-        Arrays.sort(dungeons, (a, b) -> Integer.compare(a[1], b[1]));
+        모두 탐색을 하면서 visited의 최대값을 구하면 된다.
+        * */
 
-        for (int i = 0; i < dungeons.length; i++) {
-            if (currentStamina >= dungeons[i][0]) {
-                // 현재 피로도가 해당 던전을 탐험하기에 충분하면
-                currentStamina -= dungeons[i][1]; // 던전을 탐험하고 소모 피로도만큼 감소
-                answer++;
-            } else {
-                // 던전을 탐험하기에 피로도가 부족하면 종료
-                break;
+        boolean[] visited = new boolean[dungeons.length];
+        dfs(k, dungeons, visited);
+
+        return max;
+
+    }
+
+    int max = Integer.MIN_VALUE;
+    public void dfs(int k, int[][] dungeons, boolean[] visited) {
+        int count = 0;
+        for(int i = 0; i<visited.length; i++) {
+            if(visited[i]) {
+                count++;
             }
         }
+        if(count > max) {
+            max = count;
+        }
 
-        return answer;
+
+        for(int i = 0; i<dungeons.length; i++) {
+            if(!visited[i]) {
+                if(dungeons[i][0] <= k) {
+                    visited[i] = true;
+                    k -= dungeons[i][1];
+                    dfs(k, dungeons, visited);
+                    k += dungeons[i][1];
+                    visited[i] = false;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
